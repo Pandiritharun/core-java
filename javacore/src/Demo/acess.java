d0fb3a2380ce27d698b5dea1e7a14007b7ff284d
DELL <DELL@Tharun>
2025-01-31 11:48:03 +0530

    first

---
+++ javacore/src/Demo/acess.java
@@ -0,0 +1,11 @@
+package Demo;
+
+public class acess {
+int a=5;
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+acess s=new acess();
+System.out.println(s.a);
+	}
+
+}
---
+++ javacore/src/com/tnsif/constructerdemo/Constructor.java
@@ -0,0 +1,17 @@
+package com.tnsif.constructerdemo;
+
+import java.util.Scanner;
+
+public class Constructor {
+	public static void main(String[] args) {
+		
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the customer name");
+		
+		String name=sc.next();
+		System.out.println("enter the customer id");
+		
+		
+	}
+
+}
